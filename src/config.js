679ffb5ce18a07a21c89ea748a16a9343d28ea0d
J B <bracksjohnp@gmail.com>
2025-08-10 15:36:50 +1000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const XRPL_ISSUER='rEXAMPLEISSUERADDRESS';export const XRPL_CURRENCY='GDC';export const XRPL_TRUSTLINE_LIMIT='1000000000';export const BUY_URL='';
+export const XRPL_ISSUER = 'TBA';
+export const XRPL_CURRENCY = 'GDC';
+export const XRPL_TRUSTLINE_LIMIT = '1000000000';
+export const BUY_URL = 'https://firstledger.net';
